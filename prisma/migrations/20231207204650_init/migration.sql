12b957d47ac8778b41e87b2504fb74eb59b51777
Maxwell Aboagye <nanabaah12@gmail.com>
2023-12-07 22:00:46 +0100

    using local database

--- prisma/migrations/20231207204650_init/migration.sql
+++ prisma/migrations/20231207204650_init/migration.sql
@@ -1,16 +1,15 @@
 -- CreateTable
 CREATE TABLE "Form" (
     "id" SERIAL NOT NULL,
-    "userID" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "published" BOOLEAN NOT NULL DEFAULT false,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL DEFAULT '',
-    "content" TEXT NOT NULL DEFAULT '{}',
-    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL DEFAULT '[]',
     "visits" INTEGER NOT NULL DEFAULT 0,
     "submissions" INTEGER NOT NULL DEFAULT 0,
-    "sharedUrl" TEXT NOT NULL,
+    "shareURL" TEXT NOT NULL,
 
     CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
 );
@@ -19,12 +18,17 @@ CREATE TABLE "Form" (
 CREATE TABLE "FormSubmissions" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
     "formId" INTEGER NOT NULL,
-    "content" TEXT NOT NULL DEFAULT '{}',
+    "content" TEXT NOT NULL,
 
     CONSTRAINT "FormSubmissions_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_shareURL_key" ON "Form"("shareURL");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Form_name_userId_key" ON "Form"("name", "userId");
 
 -- AddForeignKey
 ALTER TABLE "FormSubmissions" ADD CONSTRAINT "FormSubmissions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
